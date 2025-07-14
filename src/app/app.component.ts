1872c4fcf62b5b0a5a2c0bc8b03720742210e635
Raja Ahmad Zulhazmi <117263705+Zulhazmi33@users.noreply.github.com>
2025-07-14 10:10:25 +0800

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { HeaderComponent } from "./component/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   constructor() {}
---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonToolbar, IonTitle, IonHeader } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [IonToolbar, IonHeader, IonTitle],
+})
+export class HeaderComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
