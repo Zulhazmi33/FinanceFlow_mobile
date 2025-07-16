b76aab539a043d987046291f812969841a9ab4e1
Raja Ahmad Zulhazmi <117263705+Zulhazmi33@users.noreply.github.com>
2025-07-16 16:54:11 +0800

    design category

--- src/app/pages/category/category-insert/category-insert.component.ts
+++ src/app/pages/category/category-insert/category-insert.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonButtons, IonTitle, IonToolbar, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { IonHeader, IonButtons, IonTitle, IonToolbar, IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonLabel, IonInput, IonSelectOption, IonContent } from "@ionic/angular/standalone";
 import { ModalController } from '@ionic/angular/standalone';
+import { IonSelect } from '@ionic/angular/standalone'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-insert',
   templateUrl: './category-insert.component.html',
   styleUrls: ['./category-insert.component.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonToolbar, IonTitle, IonButtons, IonHeader, ]
+  imports: [IonContent, IonInput, IonLabel, IonItem, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonIcon, IonButton, IonToolbar, IonTitle, IonButtons, IonHeader, IonSelect, IonSelectOption, FormsModule]
 })
 export class CategoryInsertComponent  implements OnInit {
+
+  selectedCategory: 'Need'|'Want'|'Saving' = 'Need';
 
   constructor(
     private mcl: ModalController
